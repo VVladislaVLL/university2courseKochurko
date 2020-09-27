e2480201f8c9457d900c96ff9911ff35186e5183
VladKoch_ <vlad2002kochurko@gmail.com>
2020-09-27 13:43:00 +0300

    feat: implement destructor and costructor of Strig class, implement method *empty*

--- Lab2_Kochurko_v8/Lab2_Kochurko_v8/String.cpp
+++ Lab2_Kochurko_v8/Lab2_Kochurko_v8/String.cpp
@@ -1,8 +1,42 @@
+#include <iostream>
 #include "String.h"
+using namespace std;
+
 String::String()
 {
+	str = new char[];
 }
 
 String::~String()
 {
+	delete [] str;
 }
+
+bool String::empty() 
+{
+	return (this->length == 0) ? true : false;
+}
+//
+//char* String::get() 
+//{
+//	return this;
+//}
+
+String& String::operator=(const String& str)
+{
+    
+    return *this;
+}
+
+char& String::operator[](int index) {
+	return this->str[index];
+}
+
+
+//ostream& operator<<(ostream& out, const String& str)
+//{
+//	for (int i = 0; i < str.length; i++) {
+//		out << str[i];
+//	}
+//	return out;
+//}
--- Lab2_Kochurko_v8/Lab2_Kochurko_v8/String.h
+++ Lab2_Kochurko_v8/Lab2_Kochurko_v8/String.h
@@ -1,11 +1,19 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class String
 {
+	friend ostream& operator<<(ostream& out, const String& str);
 public:
 	String();
 	~String();
+	bool empty();
+	char* get();
+	char* set();
 
+	char& operator[](int index);
 private:
-
+	int length;
+	char* str;
 };
 
