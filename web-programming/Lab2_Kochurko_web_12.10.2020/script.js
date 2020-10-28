adccd6ead1c1ed8ae36f346abc39f77a8da27547
VladKoch_ <vlad2002kochurko@gmail.com>
2020-10-28 22:05:18 +0300

    fix: module

--- web-programming/Lab2_Kochurko_web_12.10.2020/script.js
+++ web-programming/Lab2_Kochurko_web_12.10.2020/script.js
@@ -6,73 +6,93 @@ function Student(name, surname, age, averageScore) {
     this.averageScore = averageScore;
 }
 
-// Create array of students:
-let studentArray = [];
-studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
-studentArray.push(new Student('Sergey', 'Harlanov', 18, 8.5));
-studentArray.push(new Student('Vlad', 'Mironchuk', 19, 8.0));
-studentArray.push(new Student('Vlad', 'Rabetz', 18, 8.2));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
-studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
 
 let body = document.querySelector('body');
 
 
-// const module = (function () {
-    
+const module = (function () {
+    // Create array of students:
+    let studentArray = [];
+    studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
+    studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
+    studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
+    studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
+    studentArray.push(new Student('Vlad', 'Kochurko', 18, 8.8));
+    studentArray.push(new Student('Sergey', 'Harlanov', 18, 8.5));
+    studentArray.push(new Student('Sergey', 'Harlanov', 18, 8.5));
+    studentArray.push(new Student('Sergey', 'Harlanov', 18, 8.5));
+    studentArray.push(new Student('Sergey', 'Harlanov', 18, 8.5));
+    studentArray.push(new Student('Vlad', 'Mironchuk', 19, 8.0));
+    studentArray.push(new Student('Vlad', 'Mironchuk', 19, 8.0));
+    studentArray.push(new Student('Vlad', 'Mironchuk', 19, 8.0));
+    studentArray.push(new Student('Vlad', 'Mironchuk', 19, 8.0));
+    studentArray.push(new Student('Vlad', 'Rabetz', 18, 8.2));
+    studentArray.push(new Student('Vlad', 'Rabetz', 18, 8.2));
+    studentArray.push(new Student('Vlad', 'Rabetz', 18, 8.2));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
+    studentArray.push(new Student('Jan', 'Hidronovich', 18, 10.0));
 
-// }())
+    return {
+        createTable: () => {
+            let table = document.createElement('table');
+            table = body.appendChild(table);
+            let keys = Object.keys(studentArray[0]);
 
-
-
-
-
-let table = document.createElement('table');
-    table = body.appendChild(table);
-let keys = Object.keys(studentArray[0]);
-
-let values = [];
-for (let k  = 0; k < studentArray.length; k++) {
-    values.push(Object.values(studentArray[k]));
-}
-// Create table:
-for (let i = 0; i < studentArray.length + 1; i++) {
-    let tr = document.createElement('tr');
-    tr = table.appendChild(tr);
-    if (i === 0) {
-        for (let j = 0; j < keys.length; j++) {
-            let th = document.createElement('th');
-            th = tr.appendChild(th);
-            th.innerText = keys[j];
-        }
-    }
-    else {
-        let m = 0;
-        for (let j = 0; j < keys.length; j++) {
-            let td = document.createElement('td');
-            td = tr.appendChild(td);
-            td.innerText = values[i - 1][m];
-            if (m === 3) {
-                td.className = "averageScore";
+            let values = [];
+            for (let k  = 0; k < studentArray.length; k++) {
+                values.push(Object.values(studentArray[k]));
             }
-            m++;
+            // Create table:
+            for (let i = 0; i < studentArray.length + 1; i++) {
+                let tr = document.createElement('tr');
+                tr = table.appendChild(tr);
+                if (i === 0) {
+                    for (let j = 0; j < keys.length; j++) {
+                        let th = document.createElement('th');
+                        th = tr.appendChild(th);
+                        th.innerText = keys[j];
+                    }
+                }
+                else {
+                    let m = 0;
+                    for (let j = 0; j < keys.length; j++) {
+                        let td = document.createElement('td');
+                        td = tr.appendChild(td);
+                        td.innerText = values[i - 1][m];
+                        if (m === 3) {
+                            td.className = "averageScore";
+                        }
+                        m++;
+                    }
+                }
+            }
+        },
+        getAverageMark: () => {
+            //Count average score:
+            let allTr = document.querySelectorAll('.averageScore');
+            let average = 0;
+            for (let i = 0; i < allTr.length; i++) {
+                average += +allTr[i].innerText;
+                console.log(+allTr[i].innerText);
+            }
+            let h1 = document.createElement('h1');
+            h1 = body.appendChild(h1);
+            h1.innerText = (average / allTr.length).toFixed(4);
         }
     }
-}
+}())
 
 
-//Count average score:
-let allTr = document.querySelectorAll('.averageScore');
-let average = 0;
-for (let i = 0; i < allTr.length; i++) {
-    average += +allTr[i].innerText;
-    console.log(+allTr[i].innerText);
-}
-let h1 = document.createElement('h1');
-h1 = body.appendChild(h1);
-h1.innerText = (average / allTr.length).toFixed(4);
+
+module.createTable();
+module.getAverageMark();
+
+
+
+
+
